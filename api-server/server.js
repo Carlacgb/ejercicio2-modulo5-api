e866abbbe5bce4cb15fe822fe7b8600d36b2cf63
Caita <carlacgb@gmail.com>
2025-01-29 21:16:18 -0300

    actualizar usuarios

--- api-server/server.js
+++ api-server/server.js
@@ -4,31 +4,36 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
-app.get("/PePitolindo",(request,response) => {
+app.get("/PePitolindo", (request, response) => {
     response.status(200).json({
         name: "Manz",
         life: 99,
         power: 10,
-      })
+    })
 })
-app.post("/login",(request,response) => {
+app.post("/login", (request, response) => {
     console.log(request.body)
-    const user=request.body
-    if(user.name == "doctor1" && user.password == "password1") {
+    const user = request.body
+    if (user.name == "doctor2" && user.password == "password3") {
         response.status(200).json({
             status: "ok",
-          })
-    
+        })
+
     }
-    else(
+    else if (user.name == "doctor1" && user.password == "password1") {
+        response.status(200).json({
+            status: "ok",
+        })
+    }
+    else (
         response.status(200).json({
             status: "failed",
-          })
+        })
     )
-    
+
 })
 
 app.use((err, req, res, next) => {
-  res.status(err.status).json(err);
+    res.status(err.status).json(err);
 });
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
